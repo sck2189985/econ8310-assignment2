291c7d75b1cbde3807c5f61ed5c6ed02943eacb8
sck2189985 <shelbykrz1@gmail.com>
2026-03-16 19:52:00 -0500

    Update assignment2.py
    attempt 10

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,32 @@
 import pandas as pd
 from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 training = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+
+training['DateTime'] = pd.to_datetime(training['DateTime'])
+training['hour'] = training['DateTime'].dt.hour
+
+x = training.drop(['meal', 'DateTime', 'id'], axis=1)
+
+y = training['meal']
+
+model = XGBClassifier(n_estimators=200, max_depth=3, learning_rate=0.5, objective='binary:logistic')
+
+modelFit = model.fit(x, y)
+
+test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
+
+test['DateTime'] = pd.to_datetime(test['DateTime'])
+test['hour'] = test['DateTime'].dt.hour
+
+yt = test['meal']
+
+xt = test.drop(['meal', 'DateTime', 'id'], axis=1)
+
+pred = model.predict(xt)
+
+pred.astype(float)
+
+print("test")
